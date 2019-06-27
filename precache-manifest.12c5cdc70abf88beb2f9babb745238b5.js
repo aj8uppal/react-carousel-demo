87c3e6821c99ec85abaf303c09d7d638d9a386dc
A.J. Uppal <ajuppal@AJs-MacBook-Pro.localdomain>
2019-06-26 18:34:05 -0700

    Updates

---
+++ precache-manifest.12c5cdc70abf88beb2f9babb745238b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9442cece667b2b394264e8a98f1a48b1",
+    "url": "/demos/ReactCarouselDemo/index.html"
+  },
+  {
+    "revision": "46acfa080c6982362a33",
+    "url": "/demos/ReactCarouselDemo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3969c9fc459568020634",
+    "url": "/demos/ReactCarouselDemo/static/js/2.41b3610a.chunk.js"
+  },
+  {
+    "revision": "46acfa080c6982362a33",
+    "url": "/demos/ReactCarouselDemo/static/js/main.f745dbcc.chunk.js"
+  },
+  {
+    "revision": "415d35b08060bfe9c9a0",
+    "url": "/demos/ReactCarouselDemo/static/js/runtime~main.15354e4e.js"
+  }
+]);
